fc6e1f773012a85b38d6ee348f0a0dafc8412cd8
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-11-12 22:42:49 -0500

    final call

--- CG_hw4/hw4.bash
+++
@@ -1,7 +0,0 @@
-python3.7 CG_hw4.py> example1_default.iv
-python3.7 CG_hw4.py -u 21 -v 11 -F > example1_flat.iv
-python3.7 CG_hw4.py -u 21 -v 11 -S > example1_Smooth.iv
-python3.7 CG_hw4.py -u 16 -v 9 -r 0.1 -t 0.23 > example2_flat.iv
-python3.7 CG_hw4.py -u 16 -v 9 -r 0.1 -t 0.23 -S > example2_smooth.iv
-python3.7 CG_hw4.py -u 23 -v 11 -A 1.9 -B 2.1 -C .5 -r 1.9 -t 1.6 > example3_flat.iv
-python3.7 CG_hw4.py -u 23 -v 11 -A 1.9 -B 2.1 -C .5 -r 1.9 -t 1.6 -S > example3_smooth.iv
