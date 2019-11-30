28a3f909af6c1f4d18180530f47e2c966b0f217f
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-11-29 20:56:20 -0500

    testing example without the cricles

--- CG_hw5/CG_HW5.py
+++ CG_hw5/CG_HW5.py
@@ -7,12 +7,6 @@ from enum import Enum
 import argparse
 import numpy as np
 
-class surface_options(Enum):
-    '''
-    just a way to keep things formal
-    '''
-    flat = 1
-    smooth = 2
 
 class given_args():
     '''
@@ -40,67 +34,6 @@ def parse_input(given_input, default, to_number=False):
         return float(given_input)
     return given_input
 
-
-def define_triangles(blended_matrix, given_args_params):
-    '''
-    creates the triangles of the 3d shape.
-    '''
-    the_triangles = []
-    triangle_calculation = lambda x, y, z: x * y + z
-    for i in range(0, given_args_params.v_points_triangles_len):
-        for j in range(0, given_args_params.u_points_triangles_len):
-            triangle_1 = [
-                triangle_calculation(given_args_params.u_point, i, j),
-                triangle_calculation(given_args_params.u_point, i, j + 1),
-                triangle_calculation(given_args_params.u_point, i + 1, j + 1),
-            ]
-            triangle_2 = [
-                triangle_calculation(given_args_params.u_point, i, j),
-                triangle_calculation(given_args_params.u_point, i + 1, j + 1),
-                triangle_calculation(given_args_params.u_point, i + 1, j),
-            ]
-            the_triangles.append(triangle_1)
-            the_triangles.append(triangle_2)
-    return the_triangles
-
-def list_to_string(input_list, suffix=',', to_float=True):
-    '''
-    converts all the misc list into something useable
-    by the iv file.
-    '''
-    string_builder = ''
-    if to_float:
-        for element in input_list:
-            string_builder += '{0:.6f} {1:.6f} {2:.6f}{3}\r\n'.format(
-                element[0],
-                element[1],
-                element[2],
-                suffix
-            )
-    else:
-        for element in input_list:
-            string_builder += '{0},{1},{2},{3}\r\n'.format(
-                str(int(element[0])),
-                str(int(element[1])),
-                str(int(element[2])),
-                suffix
-            )
-    return string_builder
-
-def list_to_string_normal(input_list):
-    '''
-    converts the normal list into something
-    the iv file can interpret.
-    '''
-    string_builder = ''
-    for element in input_list:
-        string_builder += '{0:.6f} {1:.6f} {2:.6f},\r\n'.format(
-            element[0],
-            element[1],
-            element[2]
-        )
-    return string_builder
-
 def Translate(axis_x=0.0, axis_y=0.0, axis_z=0.0 ):
     return [
         [1.0, 0.0, 0.0, axis_x],
@@ -120,6 +53,7 @@ def get_degrees(theta):
     degrees['minus_sin'] = get_sin(theta) * -1.0
     degrees['cos'] = get_cos(theta)
     degrees['minus_cos'] = get_cos(theta) * -1.0
+    return degrees
 
 
 def axis_x_rotation(degrees):
@@ -146,12 +80,112 @@ def axis_z_rotation(degrees):
         [0.0, 0.0, 0.0, 1.0]
     ]
 
+def define_model(length):
+    return [
+        [0.5, 0.5, length, 1], 
+        [-0.5, 0.5, length, 1], 
+        [-0.5, -0.5, length, 1], 
+        [0.5, -0.5, length, 1], 
+        [0.5, 0.5, 0, 1], 
+        [-0.5, 0.5, 0, 1], 
+        [-0.5, -0.5, 0, 1], 
+        [0.5, -0.5, 0, 1]
+    ]
 
+def create_model(translation, rotation, length):
+    model = np.array(define_model(length))
+    translation = np.array(translation)
+    rotation = np.array(rotation)
+    dot_product_translation_rotation = translation.dot(rotation)
+    return np.matmul(model, dot_product_translation_rotation)[:,[0,1,2]]
 
+def create_separator(input_list):
+    string_builder = '''Separator {
+  Coordinate3 {
+    point ['''
+    for element in input_list:
+        string_builder += '{0:.6f} {1:.6f} {2:.6f},\r\n'.format(
+            element[0],
+            element[1],
+            element[2]
+        )
+    string_builder += '''
+            ]
+        }
+    IndexedLineSet {
+    coordIndex [
+	 0, 1, 2, 0, -1,
+	 0, 2, 3, 0, -1,
+	 7, 6, 5, 7, -1,
+	 7, 5, 4, 7, -1,
+	 0, 3, 7, 0, -1,
+	 0, 7, 4, 0, -1,
+	 1, 5, 6, 1, -1,
+	 1, 6, 2, 1, -1,
+	 0, 4, 5, 0, -1,
+	 0, 5, 1, 0, -1,
+	 3, 2, 6, 3, -1,
+	 3, 6, 7, 3, -1
+    ]
+  }
+}
+    '''
+    return string_builder
 
+def create_file(box_1, box_2, box_3):
+    string_builder = '''Separator {
+  Coordinate3 {
+    point [
+	 -2.000000 -2.000000 0.000000,
+	 -2.000000 2.000000 0.000000,
+	 2.000000 2.000000 0.000000,
+	 2.000000 -2.000000 0.000000,
+	 -2.000000 -2.000000 1.000000,
+	 -2.000000 2.000000 1.000000,
+	 2.000000 2.000000 1.000000,
+	 2.000000 -2.000000 1.000000,
+    ]
+  }
+  IndexedLineSet {
+    coordIndex [
+	 0, 1, 2, 0, -1,
+	 0, 2, 3, 0, -1,
+	 7, 6, 5, 7, -1,
+	 7, 5, 4, 7, -1,
+	 0, 3, 7, 0, -1,
+	 0, 7, 4, 0, -1,
+	 1, 5, 6, 1, -1,
+	 1, 6, 2, 1, -1,
+	 0, 4, 5, 0, -1,
+	 0, 5, 1, 0, -1,
+	 3, 2, 6, 3, -1,
+	 3, 6, 7, 3, -1
+    ]
+  }
+}
+'''
+    string_builder += create_separator(box_1)
+    string_builder += create_separator(box_2)
+    string_builder += create_separator(box_3)
+    print(string_builder)
 
 def main(given_args_params):
+    #translate
+    box_1_translation = Translate(axis_z=given_args_params.lenght_1)
+    box_2_translation = Translate(axis_z=given_args_params.lenght_2)
+    box_3_translation = Translate(axis_z=given_args_params.lenght_3)
+    #rotate
+    box_1_rotation = axis_z_rotation(get_degrees(given_args_params.t1_angle))
+    box_2_rotation = axis_y_rotation(get_degrees(given_args_params.u2_angle))
+    box_3_rotation = axis_y_rotation(get_degrees(given_args_params.v3_angle))
+    #boxes
+    box_1 = create_model(box_1_translation, box_1_rotation, given_args_params.lenght_1)
+    box_2 = create_model(box_2_translation, box_2_rotation, given_args_params.lenght_2)
+    box_3 = create_model(box_3_translation, box_3_rotation, given_args_params.lenght_3)
+    #circles
 
+    #output
+    create_file(box_1, box_2, box_3)
 
 
 if __name__ == '__main__':
